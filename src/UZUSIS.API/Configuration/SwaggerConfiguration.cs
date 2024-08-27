bdbe85c9f26add9c6518b2c2cc39309afe0a5381
Gabriel Richard <gabrielrichard.contato@gmail.com>
2024-08-27 20:17:05 -0300

    upd basic

--- src/UZUSIS.API/Configuration/SwaggerConfiguration.cs
+++ src/UZUSIS.API/Configuration/SwaggerConfiguration.cs
@@ -15,7 +15,7 @@ public static class SwaggerConfiguration
         
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "UZUSIS.API", Version = "v1" });
 
             c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
