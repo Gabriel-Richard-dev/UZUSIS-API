823d957c6831eef9539427b50a94fd0f0b766ed8
Gabriel Richard <gabrielrichard.contato@gmail.com>
2024-09-02 19:08:23 -0300

    validações criadas

---
+++ src/UZUSIS.Domain/Validations/ClienteValidation.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using UZUSIS.Domain.Entities;
+
+namespace UZUSIS.Domain.Validations;
+
+public class ClienteValidation : AbstractValidator<Cliente>
+{
+    public ClienteValidation()
+    {
+        RuleFor(cliente => cliente.CPF)
+            .NotEmpty().WithMessage("O CPF é obrigatório.")
+            .Length(11).WithMessage("O CPF deve ter 11 caracteres.")
+            .Matches(@"^\d{11}$").WithMessage("O CPF deve conter apenas números e ter 11 dígitos.");
+
+        RuleFor(cliente => cliente.Celular)
+            .NotEmpty().WithMessage("O número de celular é obrigatório.")
+            .Matches(@"^\d{11}$").WithMessage("O número de celular deve conter 11 dígitos.");
+
+        RuleFor(cliente => cliente.DataNascimento)
+            .NotEmpty().WithMessage("A data de nascimento é obrigatória.")
+            .Must(data => data < DateTime.Now).WithMessage("A data de nascimento deve ser anterior à data atual.");
+
+        RuleFor(cliente => cliente.TipoUsuario)
+            .IsInEnum().WithMessage("O tipo de usuário é inválido.");
+
+        RuleFor(cliente => cliente.Nome)
+            .NotEmpty().WithMessage("O nome é obrigatório.")
+            .MaximumLength(100).WithMessage("O nome deve ter no máximo 100 caracteres.");
+
+        RuleFor(cliente => cliente.Email)
+            .NotEmpty().WithMessage("O email é obrigatório.")
+            .EmailAddress().WithMessage("O email informado é inválido.")
+            .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$").WithMessage("O email informado é inválido.");
+
+        RuleFor(cliente => cliente.Senha)
+            .NotEmpty().WithMessage("A senha é obrigatória.")
+            .MinimumLength(4).WithMessage("A senha deve ter no mínimo 4 caracteres.");
+        
+    }
+}
---
+++ src/UZUSIS.Domain/Validations/ProdutoValidation.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using UZUSIS.Domain.Entities;
+
+namespace UZUSIS.Domain.Validations;
+
+public class ProdutoValidation : AbstractValidator<Produto>
+{
+    public ProdutoValidation()
+    {
+        RuleFor(produto => produto.Nome)
+            .NotEmpty().WithMessage("O nome do produto é obrigatório.")
+            .MaximumLength(100).WithMessage("O nome do produto deve ter no máximo 100 caracteres.");
+
+        RuleFor(produto => produto.Preco)
+            .GreaterThan(0).WithMessage("O preço do produto deve ser maior que zero.");
+
+        RuleFor(produto => produto.Categoria)
+            .IsInEnum().WithMessage("A categoria do produto é inválida.");
+
+        RuleFor(produto => produto.Descricao)
+            .NotEmpty().WithMessage("A descrição do produto é obrigatória.")
+            .MaximumLength(500).WithMessage("A descrição do produto deve ter no máximo 500 caracteres.");
+    }
+}
